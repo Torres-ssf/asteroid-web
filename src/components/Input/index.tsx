6b2f15f1b3cdbce79470bca313fe65ad2eada9db
Torres-ssf <sergio.uft@gmail.com>
2020-11-03 15:07:13 -0300

    feat: implement SignIn component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,46 @@
+import React, {
+  useRef,
+  useState,
+  useCallback,
+  InputHTMLAttributes,
+} from 'react';
+
+import { IconBaseProps } from 'react-icons/lib/cjs';
+
+import { Container } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ComponentType<IconBaseProps>;
+  error?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({ icon: Icon, error, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  return (
+    <Container isFocused={isFocused} isFilled={isFilled} isErrored={!!error}>
+      {Icon && <Icon size={20} />}
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default Input;
