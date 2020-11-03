14f9b7aa87496fcdee3568ea1909bf7b73435b87
Torres-ssf <sergio.uft@gmail.com>
2020-11-03 15:07:02 -0300

    style: implement SignIn component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,56 @@
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  display: flex;
+  color: #666360;
+  align-items: center;
+  padding: 16px;
+  width: 100%;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #30c5ff;
+      color: #30c5ff;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #30c5ff;
+    `}
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #f4ede8;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
