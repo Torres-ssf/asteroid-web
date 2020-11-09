1b580ff97b59c999421a26767faa180d5f67babf
Torres-ssf <sergio.uft@gmail.com>
2020-11-09 16:03:22 -0300

    feat: add main route at routes component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Switch } from 'react-router-dom';
 import AppRoute from './AppRoute';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Main from '../pages/Main';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
@@ -10,6 +11,7 @@ const Routes: React.FC = () => (
     <AppRoute path="/" exact component={SignIn} />
     <AppRoute path="/signup" component={SignUp} />
 
+    <AppRoute path="/main" component={Main} isPrivate />
     <AppRoute path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
