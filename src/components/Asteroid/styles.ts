3f3efa35f4cafd40dbc0452594a8874c65ab5d1e
Torres-ssf <sergio.uft@gmail.com>
2020-11-09 16:06:18 -0300

    style: implement Asteroid component

---
+++ src/components/Asteroid/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.a`
+  background: #f4ede8;
+  border-radius: 4px;
+  color: #312e38;
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 1rem;
+  justify-content: center;
+  position: relative;
+  padding: 30px;
+  margin: 2px;
+`;
+
+export const ListNumber = styled.span`
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  display: flex;
+  justify-content: center;
+  font-weight: 500;
+`;
