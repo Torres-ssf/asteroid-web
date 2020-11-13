f58601efc130d2277a155d7ed3caa59cef2e1699
Torres-ssf <sergio.uft@gmail.com>
2020-11-13 17:47:13 -0300

    style: add pagination functionality

--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -87,4 +87,40 @@ export const AsteroidsList = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
+
+  ul {
+    display: flex;
+    margin: 10px 0 0;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    @media (max-width: 600px) {
+      font-size: 0.9rem;
+    }
+
+    li {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 60px;
+
+      a {
+        border: none;
+        border-radius: 5px;
+        color: #3899bf;
+        cursor: pointer;
+        margin: 6px;
+        padding: 8px 12px;
+      }
+    }
+
+    li.active a {
+      color: white;
+      background-color: #3899bf;
+    }
+
+    li.disabled {
+      display: none;
+    }
+  }
 `;
