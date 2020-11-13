92ce07ec747a45409cad16aba236bcddf896a5a0
Torres-ssf <sergio.uft@gmail.com>
2020-11-13 20:40:01 -0300

    style: improve Asteroid component styles

--- src/components/Asteroid/styles.ts
+++ src/components/Asteroid/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.article`
   background: #f4ede8;
   border-radius: 4px;
   color: #312e38;
@@ -9,9 +9,21 @@ export const Container = styled.div`
   font-size: 1rem;
   justify-content: center;
   position: relative;
-  padding: 30px;
-  margin: 2px;
+  padding: 40px;
+  margin: 5px 0;
   max-width: 1200px;
+
+  @media (max-width: 700px) {
+    padding: 30px;
+  }
+
+  @media (max-width: 550px) {
+    padding: 20px;
+  }
+
+  @media (max-width: 500px) {
+    margin: 2.5px 0;
+  }
 `;
 
 export const ListNumber = styled.span`
@@ -20,5 +32,6 @@ export const ListNumber = styled.span`
   left: 20px;
   display: flex;
   justify-content: center;
+  font-size: 0.9rem;
   font-weight: 500;
 `;
