ccfffd6fe921c733d970f8142bf70d05eadc17b0
Torres-ssf <sergio.uft@gmail.com>
2020-11-16 13:49:48 -0300

    feat: add link to update user at Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower } from 'react-icons/fi';
+import { FaCog } from 'react-icons/fa';
 import { Container, UserContainer } from './styles';
 
 interface HeaderProps {
   userName: string;
+  profilePage?: boolean;
 }
 
-const Header: React.FC<HeaderProps> = ({ userName }) => {
+const Header: React.FC<HeaderProps> = ({ userName, profilePage }) => {
   return (
     <Container>
       <div>
         <UserContainer>
           Welcome,
           <Link to="/profile">{userName}</Link>
+          {profilePage && (
+            <Link to="/" title="Update profile">
+              <FaCog size={22} color="#F4EDE8" />
+            </Link>
+          )}
         </UserContainer>
 
         <button title="Sign out" type="button">
-          <FiPower size={20} color="#fff" />
+          <FiPower size={22} color="#fff" />
         </button>
       </div>
     </Container>
