7a345333be7f4f85c58040095b47312c2ea74120
Torres-ssf <sergio.uft@gmail.com>
2020-11-16 14:31:49 -0300

    refactor: add Profile page to index routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,9 @@ import { Switch } from 'react-router-dom';
 import AppRoute from './AppRoute';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+
 import Main from '../pages/Main';
-import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -12,7 +13,7 @@ const Routes: React.FC = () => (
     <AppRoute path="/signup" component={SignUp} />
 
     <AppRoute path="/main" component={Main} isPrivate />
-    <AppRoute path="/dashboard" component={Dashboard} isPrivate />
+    <AppRoute path="/me" component={Profile} isPrivate />
   </Switch>
 );
 
